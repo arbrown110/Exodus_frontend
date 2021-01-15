f52ca6b818c63d1f339078ef2f8a387ed62ba8a6
Angel_Brown <arbrown110@gmail.com>
2021-01-15 09:22:46 -0500

    creation of adventure actions.

---
+++ src/actions/AdventuresActions.js
@@ -0,0 +1,66 @@
+//Fetching adventures
+
+export const fetchAdventures = () => {
+    return dispatch => {
+        dispatch({type: 'LOADING_ADVENTURES'})
+       return fetch('api/v1/adventures')
+        .then(res => res.json())
+        .then(adventures => dispatch({type: 'ADVENTURES_LOADED', payload: adventures})) 
+    }
+}
+
+
+// fetching  adventures's id
+
+export const fetchAdventure = id => {
+    return (dispatch) => {
+        dispatch({type: 'LOADING_ADVENTURES'})
+        return fetch('/api/v1/adventures/${id}')
+        .then(res => res.json())
+        .then(adventure => dispatch({type: 'ADVENTURE_LOADED', payload: adventure}))
+    }
+}
+
+
+// adding adventure
+
+export const addAdventure = adventureForm => {
+    let data = {
+        method: 'POST',
+        Headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(adventureForm)
+    }
+    return dispatch => {
+        fetch('/api/v1/adventures', data)
+        .then(res => res.json())
+        .then(adventure => dispatch({
+            type: 'CREATE_ADVENTURE',
+            payload: adventure
+        }))
+    }
+}
+
+
+// deleting adventure
+
+export const deleteAdventure = adventure_id =>{
+    let data = {
+        method: 'DELETE',
+        headers: {
+            'Accept': 'application/json',
+            'Contet-Type': 'application/json'
+        }
+    }
+    return dispatch => {
+        fetch('/api/v1/${adventure_id', data)
+        .then(res => res.json())
+        .then(adventure => dispatch({
+            type: 'DELETE_ADVENTURE',
+            payload: adventure
+        }))
+    }
+}
+
