909b2f7d575efa120cfb495da6fe4ee3068613c1
Angel_Brown <arbrown110@gmail.com>
2021-01-15 18:16:08 -0500

    creating reducers.

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+import adventureReducer from './adventureReducer'
+
+
+const rootReducer = combineReducers({
+    adventureReducer
+})
+
+export default rootReducer;
